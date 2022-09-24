75d3786c829e2f87e219324e75e8b1c237f33101
Evgeny Bershdsky <79035534+jayber-dev@users.noreply.github.com>
2022-09-24 21:25:47 +0300

    add

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 from flask import Flask, render_template
 import sqlite3 as sq
-import datetime
+from datetime import datetime
 
 conn = sq.connect('data.db')
 
 cur = sq.Cursor(conn)
 
-cur.execute('''CREATE TABLE IF NOT EXISTS warehouse (
-    warehouse_name TEXT NOT NULL, 
-    item TEXT NOT NULL,
-    item_description TEXT,
-    quantity INTEGER,
-    catalog_id TEXT,
-    update_date TEXT,
-    WITHOUT ROW)
-    ''');
+# cur.execute('''CREATE TABLE IF NOT EXISTS warehouse (
+#     warehouse_name TEXT NOT NULL, 
+#     item TEXT NOT NULL,
+#     item_description TEXT,
+#     quantity INTEGER,
+#     catalog_id TEXT,
+#     update_date TEXT,
+#     WITHOUT ROW)
+#     ''');
 
+
+
+
+
+# cur.execute(f'''INSERT INTO warehouse (warehouse_name,item,item_description,quantity,catalog_id,update_date)
+#             VALUES ('931','מסור','חד מאוד',5,'50000321','{datetime.now()}')''')
 conn.commit()
 
 
-
-cur.execute(f'''INSERT INTO warehouse (warehouse_name,item,item_description,quantity,catalog_id,update_date)
-            VALUES ('931','מסור','חד מאוד',5,'50000321','{datetime.datetime().now()}')''')
-conn.commit()
 
 app = Flask(__name__)
 

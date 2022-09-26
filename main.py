77582f12a96e5ec7be5fa23017db1264d12dc953
Evgeny Bershdsky <79035534+jayber-dev@users.noreply.github.com>
2022-09-26 22:04:02 +0300

    finshed

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import sqlite3 as sq
 import datetime
 
 conn = sq.connect('data.db')
-
 cur = sq.Cursor(conn)
 
 cur.execute('''CREATE TABLE IF NOT EXISTS warehouse (
@@ -16,15 +15,7 @@ cur.execute('''CREATE TABLE IF NOT EXISTS warehouse (
     update_date text)
     ''');
 
-
-
-# cur.execute(f'''INSERT INTO warehouse (warehouse_name,item,item_description,quantity,catalog_id,update_date)
-#             VALUES ('931','צבע','צבע שצובע הכל ויחד עם זאת זה לא צובע טוב ',5,'50000321','{datetime.now()}')''')
-
-
 conn.commit()
-
-
 
 app = Flask(__name__)
 
@@ -86,6 +77,11 @@ def item_update(row_id):
     print(request.args.to_dict())
     return render_template('updateItem.html', row_id=row_id, data=request.args.to_dict())
         
-    
+@app.route('/search/<q>', methods=["GET","POST"])
+def search(q):
+    print(request.args)
+    pass
+
+
 if (__name__ == '__main__'):
     app.run(debug=True)

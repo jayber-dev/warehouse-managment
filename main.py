6139c97b6fb6a87e791e6bad62236a21010c745a
jay ber <jayber1@gmail.com>
2022-10-24 14:46:28 +0300

    little fixes

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ cur.execute('''CREATE TABLE IF NOT EXISTS warehouse (
     quantity INTEGER,
     catalog_id TEXT,
     update_date text)
-    ''');
+    ''')
 
 conn.commit()
 
@@ -29,8 +29,11 @@ def index():
     cur = sq.Cursor(conn)
     res = cur.execute('''SELECT * FROM warehouse''')
     data = res.fetchall()
+    warehouse_names = set()
+    for i in range(len(data)):
+        warehouse_names.add(data[i][1])
     cur.close()
-    return render_template('index.html', data=data)
+    return render_template('index.html', data=data, set_data=warehouse_names)
 
 @app.route('/addItem',methods=['GET','POST'])
 def add_item():
@@ -46,7 +49,13 @@ def add_item():
         cur.close()
         print(request.form.to_dict())
         return redirect(url_for('index'))
-    return render_template('addItem.html')
+    conn = sq.connect('data.db')
+    cur = sq.Cursor(conn)
+    res = cur.execute('SELECT warehouse_name FROM warehouse')
+    all_warehouses = res.fetchall()
+    warehouse_set = set(all_warehouses)
+    print(warehouse_set)
+    return render_template('addItem.html', data=warehouse_set)
 
 @app.route('/item_delete/<string:row_delete>',methods=['GET'])
 def item_delete(row_delete):
@@ -82,20 +91,32 @@ def item_update(row_id):
 @app.route('/search/', methods=["GET","POST"])
 def search():
     search_query = request.args.to_dict()['q']
-    print(search_query)
+    # print(search_query)
     
     conn = sq.connect('data.db')    
     cur = sq.Cursor(conn)
     if(search_query == 'All warehouses'):
         res = cur.execute('''SELECT * FROM warehouse''')
         query_data = cur.fetchall()
-        return render_template('index.html', data=query_data)
+        print(res)
+        warehouse_names = set()
+        try:
+            for i in range(len(res)):
+                warehouse_names.add(res[i][1])
+                query_data = cur.fetchall()
+        except:
+            pass
+        return render_template('index.html', data=query_data, set_data=warehouse_names)
     else:
         cur.execute("PRAGMA case_sensitive_like = true;")
         cur.execute(f"""SELECT * FROM warehouse WHERE warehouse_name LIKE '%{search_query}%' OR item LIKE '%{search_query}%' OR item_description LIKE '%{search_query}%' """)
         query_data = cur.fetchall()
+        warehouse_names = set()
+        for i in range(len(query_data) -1):
+                warehouse_names.add(query_data[i][1])
+                query_data = cur.fetchall()
         print(query_data)
-        return render_template('index.html', data=query_data, q_param=search_query)
+        return render_template('index.html', data=query_data, q_param=search_query, set_data=warehouse_names)
 
 
 if (__name__ == '__main__'):
